f4603630ed9c277456d622696cb2793bad4f3e23
BEOKS <lee01042000@gmail.com>
2025-02-20 22:26:21 +0900

    quartz 4

---
+++ quartz.config.ts
@@ -0,0 +1,95 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "🪴 Beoks Blog",
+    pageTitleSuffix: " | Beoks Blog",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: {
+      provider: "plausible",
+    },
+    locale: "ko-KR",
+    baseUrl: "quartz.jzhao.xyz",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    generateSocialImages: false,
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Schibsted Grotesk",
+        body: "Source Sans Pro",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#ffffff",
+          lightgray: "#f0f0f0",
+          gray: "#cccccc",
+          darkgray: "#666666",
+          dark: "#333333",
+          secondary: "#005f73",
+          tertiary: "#0a9396",
+          highlight: "rgba(0, 95, 115, 0.15)",
+          textHighlight: "#94d2bd88",
+        },
+        darkMode: {
+          light: "#121212",
+          lightgray: "#2c2c2c",
+          gray: "#4a4a4a",
+          darkgray: "#b0b0b0",
+          dark: "#e0e0e0",
+          secondary: "#94d2bd",
+          tertiary: "#0a9396",
+          highlight: "rgba(0, 95, 115, 0.15)",
+          textHighlight: "#005f7388",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
