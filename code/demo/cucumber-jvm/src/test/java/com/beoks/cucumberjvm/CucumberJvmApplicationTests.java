ab6b10d105f7c28849afe679d56fa92ccf7d9aee
BEOKS <lee01042000@gmail.com>
2025-06-03 20:41:21 +0900

    cucumber jvm module setting

---
+++ code/demo/cucumber-jvm/src/test/java/com/beoks/cucumberjvm/CucumberJvmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beoks.cucumberjvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberJvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
