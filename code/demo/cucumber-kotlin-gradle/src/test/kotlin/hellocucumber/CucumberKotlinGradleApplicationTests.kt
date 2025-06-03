d5d46896a6223118a636835f99cccc3f6a6b7376
BEOKS <lee01042000@gmail.com>
2025-06-03 21:30:27 +0900

    cucumber kotlin module init

---
+++ code/demo/cucumber-kotlin-gradle/src/test/kotlin/hellocucumber/CucumberKotlinGradleApplicationTests.kt
@@ -0,0 +1,13 @@
+package hellocucumber
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CucumberKotlinGradleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
